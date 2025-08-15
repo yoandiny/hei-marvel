4e3fb7f7c1a910f74eca66eb5ae13ad0377e14dd
Yoan <yoandiny14@gmail.com>
2025-08-15 21:31:25 +0300

    Enhance character management: add update and delete functionality, and improve validation for character creation

--- backend/server.js
+++ backend/server.js
@@ -29,13 +29,34 @@ app.get('/characters/:id', async(req, res)=>{
 
 app.post('/characters', async(req, res)=>{
   const character = req.body;
-  if (!character || !character.id) {
+  if (!character || !character.name || !character.realName || !character.universe) {
     return res.status(400).send('Character data is required');
   }
   await manager.createCharacter(character);
   res.status(201).send('Character created');
 });
 
+app.put('/characters/:id', async(req, res)=>{
+  const id = req.params.id;
+  const updatedCharacter = req.body;
+  if (!updatedCharacter) {
+    return res.status(400).send('Updated character data is required');
+  }
+  await manager.updateCharacter(id, updatedCharacter);
+  res.send('Character updated');
+});
+
+app.delete('/characters/:id', async(req, res)=>{
+  try {
+    const id = req.params.id;
+  await manager.deleteCharacter(id);
+  res.status(200).send('Character deleted');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting character');
+    
+  }
+});
 
 
 app.listen(8080, ()=>{
